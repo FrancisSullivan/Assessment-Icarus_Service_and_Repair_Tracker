4f58e7ed09acd272a5b6a6ca6283d1b223fbc6d6
FrancisSullivan <francissullivan94@outlook.com>
2022-11-01 10:47:22 +0800

    Basic GUI

--- Icarus_Service_and_Repair_Tracker.Designer.cs
+++ Icarus_Service_and_Repair_Tracker.Designer.cs
@@ -28,20 +28,338 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBoxStructure = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.clientName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.droneModel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.serviceProblem = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.serviceCost = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.serviceTag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBoxStructure.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clientName,
+            this.droneModel,
+            this.serviceProblem,
+            this.serviceCost,
+            this.serviceTag});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 140);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(964, 185);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(29, 394);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(964, 199);
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Client Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Service Problem";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(1029, 140);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(389, 454);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Express Queue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1024, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Complete";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Regular Queue";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(660, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Service Cost";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(832, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Service Tag";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(208, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(135, 25);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Drone Model";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(29, 66);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(178, 31);
+            this.textBoxName.TabIndex = 47;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(392, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 53);
+            this.textBox1.TabIndex = 48;
+            // 
+            // groupBoxStructure
+            // 
+            this.groupBoxStructure.Controls.Add(this.radioButton1);
+            this.groupBoxStructure.Controls.Add(this.radioButton2);
+            this.groupBoxStructure.Location = new System.Drawing.Point(1029, 24);
+            this.groupBoxStructure.Name = "groupBoxStructure";
+            this.groupBoxStructure.Size = new System.Drawing.Size(255, 66);
+            this.groupBoxStructure.TabIndex = 57;
+            this.groupBoxStructure.TabStop = false;
+            this.groupBoxStructure.Text = "Service Priority ";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(118, 29);
+            this.radioButton1.TabIndex = 36;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Regular";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(130, 30);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(121, 29);
+            this.radioButton2.TabIndex = 37;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Express";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(213, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 31);
+            this.textBox3.TabIndex = 58;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(852, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 41);
+            this.button1.TabIndex = 59;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(852, 608);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 41);
+            this.button3.TabIndex = 61;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1290, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 41);
+            this.button2.TabIndex = 62;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 691);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1449, 22);
+            this.statusStrip1.TabIndex = 63;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(665, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(156, 31);
+            this.textBox4.TabIndex = 64;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(837, 66);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(156, 31);
+            this.textBox5.TabIndex = 65;
+            // 
+            // clientName
+            // 
+            this.clientName.Text = "Client Name";
+            this.clientName.Width = 85;
+            // 
+            // droneModel
+            // 
+            this.droneModel.Text = "Drone Model";
+            this.droneModel.Width = 75;
+            // 
+            // serviceProblem
+            // 
+            this.serviceProblem.Text = "Service Problem";
+            this.serviceProblem.Width = 160;
+            // 
+            // serviceCost
+            // 
+            this.serviceCost.Text = "Service Cost";
+            this.serviceCost.Width = 75;
+            // 
+            // serviceTag
+            // 
+            this.serviceTag.Text = "Service Tag";
+            this.serviceTag.Width = 75;
             // 
             // Icarus_Service_and_Repair_Tracker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 887);
+            this.ClientSize = new System.Drawing.Size(1449, 713);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBoxStructure);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
             this.Name = "Icarus_Service_and_Repair_Tracker";
             this.Text = "Icarus_Service_and_Repair_Tracker";
+            this.groupBoxStructure.ResumeLayout(false);
+            this.groupBoxStructure.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBoxStructure;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ColumnHeader clientName;
+        private System.Windows.Forms.ColumnHeader droneModel;
+        private System.Windows.Forms.ColumnHeader serviceProblem;
+        private System.Windows.Forms.ColumnHeader serviceCost;
+        private System.Windows.Forms.ColumnHeader serviceTag;
     }
 }
 
--- Icarus_Service_and_Repair_Tracker.cs
+++ Icarus_Service_and_Repair_Tracker.cs
@@ -19,90 +19,108 @@ namespace Icarus_Service_and_Repair_Tracker
             InitializeComponent();
         }
         #endregion
+
         #region 6.01 Separate class file
         /*
         [ ]..
         */
         #endregion
+
         #region 6.02 List of Class
         /*
         [ ]..
         */
         #endregion
+
         #region 6.03 Queue of class (regular service)
         /*
         [ ]..
         */
         #endregion
+
         #region 6.04 Queue of class (express service)
         /*
         [ ]..
         */
         #endregion
+
         #region 6.05 Add new service item
         /*
         [ ]..
         */
         #endregion
+
         #region 6.06 Increase express service by 15%
         /*
         [ ]..
         */
         #endregion
+
         #region 6.07 Method to return radio button priority 
         /*
         [ ]..
         */
         #endregion
+
         #region 6.08 Method to display regular service queue in ListView
         /*
         [ ]..
         */
         #endregion
+
         #region 6.09 Method to display express service queue in ListView
         /*
         [ ]..
         */
         #endregion
+
         #region 6.10 Keypress method for service cost
         /*
         [ ]..
         */
         #endregion
+
         #region 6.11 Method to increment service tag control
         /*
         [ ]..
         */
         #endregion
+
         #region 6.12 Click to populate textbox from regular service ListView
         /*
         [ ]..
         */
         #endregion
+
         #region 6.13 Click to populate textbox from express service ListView
         /*
         [ ]..
         */
         #endregion
+
         #region 6.14 Button dequeue regular data structure, add item to list
         /*
         [ ]..
         */
         #endregion
+
         #region 6.15 Button dequeue express data structure, add item to list
         /*
         [ ]..
         */
         #endregion
+
         #region 6.16 Double-click remove item from listbox and list
         /*
         [ ]..
         */
         #endregion
+
         #region 6.17 Method to clear textboxes
         /*
         [ ]..
         */
         #endregion
+
     }
 }
